938d19b3d6bf4f3e1e22fb5d1f897953a15719b4
cxtea@foxmail.com <chengxiao1>
2023-03-30 12:05:01 +0800

    写入用户测试

--- service-passenger-user/src/main/java/com/didi2023/servicepassengeruser/service/UserService.java
+++ service-passenger-user/src/main/java/com/didi2023/servicepassengeruser/service/UserService.java
@@ -7,6 +7,7 @@ import com.didi2023.servicepassengeruser.mapper.PassengerUserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -25,7 +26,19 @@ public class UserService {
 
         System.out.println(passengerUsers.size() == 0 ? "无记录":passengerUsers.get(0).getPassengerPhone());
         //登录或者注册用户
+        if(passengerUsers.size() == 0){
+            PassengerUser passengerUser = new PassengerUser();
+            passengerUser.setPassengerName("张三");
+            passengerUser.setPassengerGender((byte)0);
+            passengerUser.setPassengerPhone(passengerPhone);
+            passengerUser.setState((byte)0);
 
+            LocalDateTime now = LocalDateTime.now();
+            passengerUser.setGmtCreate(now);
+            passengerUser.setGmtModified(now);
+
+            passengerUserMapper.insert(passengerUser);
+        }
         //判断用户是否存在  存在登录 获取token
 
         //不存在 注册并获取token
