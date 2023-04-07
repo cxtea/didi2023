640f86f401644f1bdc52a244b76af44906ae793f
cxtea@foxmail.com <chengxiao1>
2023-04-08 01:39:50 +0800

    车辆信息添加修改接口

--- service-driver-user/src/main/java/com/didi/servicedriveruser/controller/CarController.java
+++ service-driver-user/src/main/java/com/didi/servicedriveruser/controller/CarController.java
@@ -1,7 +1,11 @@
 package com.didi.servicedriveruser.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.didi.servicedriveruser.service.CarService;
+import com.didi2023.internalcommon.dto.Car;
+import com.didi2023.internalcommon.dto.ResponseResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import org.springframework.stereotype.Controller;
 
@@ -13,8 +17,25 @@ import org.springframework.stereotype.Controller;
  * @author shawn
  * @since 2023-04-08
  */
-@Controller
-@RequestMapping("/car")
+@RestController
 public class CarController {
 
+    @Autowired
+    private CarService carService;
+    @PostMapping("/car")
+    public ResponseResult addCar(@RequestBody Car car){
+
+        return carService.addCar(car);
+    }
+    @PutMapping("/car")
+    public ResponseResult updateCar(@RequestBody Car car){
+
+        return carService.updateCar(car);
+    }
+
+    @GetMapping("/car/{id}")
+    public ResponseResult getCarById(@PathVariable Integer id){
+
+        return carService.getCarById(id);
+    }
 }
--- service-driver-user/src/main/java/com/didi/servicedriveruser/service/CarService.java
+++ service-driver-user/src/main/java/com/didi/servicedriveruser/service/CarService.java
@@ -1,7 +1,41 @@
 package com.didi.servicedriveruser.service;
 
+import com.didi.servicedriveruser.mapper.CarMapper;
+import com.didi2023.internalcommon.dto.Car;
+import com.didi2023.internalcommon.dto.ResponseResult;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class CarService {
+
+    @Autowired
+    private CarMapper carMapper;
+
+    public ResponseResult addCar(Car car) {
+
+        car.setGmtCreate(LocalDateTime.now());
+        car.setGmtModified(LocalDateTime.now());
+        int insert = carMapper.insert(car);
+
+        return ResponseResult.success(insert);
+
+    }
+
+    public ResponseResult updateCar(Car car) {
+
+        car.setGmtModified(LocalDateTime.now());
+        int update = carMapper.updateById(car);
+
+        return ResponseResult.success(update);
+    }
+
+    public ResponseResult getCarById(Integer id) {
+
+        Car car = carMapper.selectById(id);
+
+        return ResponseResult.success(car);
+    }
 }
